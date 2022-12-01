f3d943adb69bd14735bafe0eb2fc26864372562c
6234456 <6234456@sina.com>
2022-12-01 21:48:06 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): List<Long> {
+        var tmp = mutableListOf<Int>()
+        return input.fold(mutableListOf<Long>()){
+            acc, s ->
+               if (s.isBlank()){
+                   acc.add(tmp.fold(0){a, i -> a + i})
+                   tmp.clear()
+               }else{
+                   tmp.add(s.toInt())
+               }
+
+                acc
+        }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input).sortedDescending().take(3).fold(0L){a, i -> a + i})
 }
